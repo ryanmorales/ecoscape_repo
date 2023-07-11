8b3480ecc097fc8a048097949b84dd30ccf48f49
RusselCuizon <91875790+RusselCuizon@users.noreply.github.com>
2023-07-11 22:49:56 +0800

    base html

--- es_calendar/urls.py
+++ es_calendar/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('calendar/', views.EsCalendar, name='es_calendar'),
+    path('', views.EsCalendar, name='es_calendar'),
     path('all_events/', views.all_events, name='all_events'), 
     path('update/', views.update, name='update'),
 
